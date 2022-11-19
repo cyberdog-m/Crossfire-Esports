c5fbbefa652af89b8725ddc25374405dff920eb8
Naveen Mathew <naveenmathew1585@gmail.com>
2022-11-19 23:49:33 +0530

    optimised ui

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="absolute mx-auto mt-10 w-full bg-purple-900/80 px-5">
+  <div class="absolute w-full px-5 mx-auto mt-10 bg-purple-900/80">
     <div class="flex items-center justify-between">
-      <div class="font-exo my-4 flex flex-col text-white/50">
+      <div class="flex flex-col my-4 font-exo text-white/50">
         <div>
           <h5 class="font-bold text-white/75">Contact Us</h5>
           <p class="text-sm"><a href="tel:+918078741585">+91 80787 41585</a></p>
         </div>
         <div class="mt-3">
           <h5 class="font-bold text-white/75">Follow Us</h5>
-          <div class="mt-2 flex">
+          <div class="flex mt-2">
             <a href="https://instagram.com/element_mcet">
               <img
                 src="../assets/instagram-logo-white.png"
                 alt="instagram link to element association"
-                class="h-7 opacity-90"
+                class="h-5 opacity-90"
               />
             </a>
             <a href="https://malabarcet.ac.in">
               <img
                 src="../assets/globe-logo-white.png"
                 alt="instagram link to element association"
-                class="ml-2 h-7 opacity-90"
+                class="h-5 ml-2 opacity-90"
               />
             </a>
           </div>
@@ -29,7 +29,7 @@
       <img
         src="../assets/malabar-logo.png"
         alt="malabar college logo"
-        class="h-16 opacity-80"
+        class="w-[12rem] opacity-80"
       />
     </div>
   </div>
