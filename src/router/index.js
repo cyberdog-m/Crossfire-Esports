5ed2882573ac07d3b928b6fa1a05428cd6b31caf
Naveen Mathew <naveenmathew1585@gmail.com>
2022-11-20 15:56:15 +0530

    dynamically change page title on each route

--- src/router/index.js
+++ src/router/index.js
@@ -8,38 +8,49 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta:{ title: 'Crossfire | Month of E-Sports' }
     },
     {
       path: "/valorant",
       name: "valorant",
       component: () => import("../views/detailView/ValorantDetail.vue"),
+      meta:{title: 'Crossfire | Valorant'}
     },
     {
       path: "/fifapc",
       name: "fifa pc",
       component: () => import("../views/detailView/FifaPcDetail.vue"),
+      meta:{ title: 'Crossfire | Fifa PC' }
     },
     {
       path: "/efootball",
       name: "efootball",
       component: () => import("../views/detailView/EFootballDetail.vue"),
+      meta:{ title: 'Crossfire | E Football' }
     },
     {
       path: "/bgmi",
       name: "bgmi",
       component: () => import("../views/detailView/BgmiDetail.vue"),
+      meta:{ title: 'Crossfire | BGMI' }
     },
     {
       path: "/forza",
       name: "forzahorizon",
       component: () => import("../views/detailView/ForzaHorizonDetail.vue"),
+      meta:{ title: 'Crossfire | Forza Horizon' }
     },
     {
       path: "/freefire",
       name: "freefire",
       component: () => import("../views/detailView/FreeFireDetail.vue"),
+      meta:{ title: 'Crossfire | Free Fire' }
     },
   ],
 });
+
+router.beforeEach((to)=>{
+  document.title = to.meta.title
+})
 
 export default router;
